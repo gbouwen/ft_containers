e37bec192819ecf8b1b60165eb8ee14adc7896e9
gbouwen <gijsbouwen@gmail.com>
2021-08-03 11:17:05 +0200

    added main.cpp + vector template

---
+++ containers/Vector.hpp
@@ -0,0 +1,39 @@
+// Vector.hpp
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace ft {
+
+	class vector {
+
+	public:
+
+	// !!! MEMBER TYPES !!!
+
+	// !!! MEMBER FUNCTIONS !!!
+
+	// --- CONSTRUCTOR/DESTRUCTOR/OPERATOR= ---
+
+	// --- ITERATORS ---
+
+	// --- CAPACITY ---
+
+	// --- ELEMENT ACCESS ---
+
+	// --- MODIFIERS ---
+
+	// --- ALLOCATOR ---
+
+	// !!! NON MEMBER FUNCTION OVERLOADS !!!
+
+	// --- RELATIONAL OPERATORS ---
+
+	// --- SWAP ---
+
+	private:
+
+	};
+};
+
+#endif
