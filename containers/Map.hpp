f19876eac16ae5e991141a23ddca0b57baf2d232
gbouwen <gijsbouwen@gmail.com>
2021-08-03 11:19:52 +0200

    added map template

---
+++ containers/Map.hpp
@@ -0,0 +1,37 @@
+// Map.hpp
+
+#ifndef MAP_HPP
+# define MAP_HPP
+
+namespace ft {
+
+	class map {
+
+	public:
+
+	// !!! MEMBER TYPES !!!
+
+	// !!! MEMBER FUNCTIONS !!!
+
+	// --- CONSTRUCTOR/DESTRUCTOR/OPERATOR= ---
+
+	// --- ITERATORS ---
+
+	// --- CAPACITY ---
+
+	// --- ELEMENT ACCESS ---
+
+	// --- MODIFIERS ---
+
+	// --- OBSERVERS ---
+
+	// --- OPERATIONS ---
+
+	// --- ALLOCATOR ---
+
+	private:
+
+	};
+};
+
+#endif
