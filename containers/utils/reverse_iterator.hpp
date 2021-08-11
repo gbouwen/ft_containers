4eb029bdbcb69fbf3a1938e1b761a7a4f39bda20
gbouwen <gijsbouwen@gmail.com>
2021-08-11 15:28:59 +0200

    start of reverse_iterator

--- containers/utils/reverse_iterator.hpp
+++ containers/utils/reverse_iterator.hpp
@@ -17,6 +17,63 @@ namespace ft {
 		typedef ft::iterator_traits<Iterator>::pointer				pointer;
 		typedef ft::iterator_traits<Iterator>::reference			reference;
 
+	protected:
+
+		iterator_type	_current;
+
+	public:
+
+	// --- CONSTRUCTORS ---
+
+		reverse_iterator(): _current() { };
+
+		explicit reverse_iterator(iterator_type it): _current(it) { };
+
+		template <class Iter>
+  		reverse_iterator(const reverse_iterator<Iter>& rev_it): _current(rev_it.base()) { };
+
+	// --- MEMBER FUNCTIONS ---
+
+		iterator_type base() const { return (_current); };
+
+		reference	operator*() const {
+			iterator_type tmp = _current;
+
+			return (*--tmp);
+		};
+
+		pointer operator->() const { return (&(operator*())); };
+
+		// prefix
+		reverse_iterator& operator++() {
+			--_current;
+
+			return (*this);
+		};
+
+		// postfix
+		reverse_iterator  operator++(int) {
+			reverse_iterator temp = *this;
+
+			--_current;
+			return (temp);
+		};
+
+		// prefix
+		reverse_iterator& operator--() {
+			++_current;
+
+			return (*this);
+		};
+
+		// postfix
+		reverse_iterator  operator--(int) {
+			reverse_iterator temp = *this;
+
+			++_current;
+			return (temp);
+		};
+
 	}; // reverse_iterator
 
 }; //namespace ft
