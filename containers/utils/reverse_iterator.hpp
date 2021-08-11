839b77d06ef243933a2152fd0deaf4ec755cbab1
gbouwen <gijsbouwen@gmail.com>
2021-08-11 19:46:17 +0200

    non member functions work

--- containers/utils/reverse_iterator.hpp
+++ containers/utils/reverse_iterator.hpp
@@ -88,6 +88,48 @@ namespace ft {
 
 	}; // reverse_iterator
 
+	template <class Iterator>
+	bool operator==(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+		return (lhs.base() == rhs.base());
+	};
+
+	template <class Iterator>
+	bool operator!=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+		return (!(lhs == rhs));
+	};
+
+	template <class Iterator>
+	bool operator<(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+		return (rhs.base() < lhs.base());
+	};
+
+	template <class Iterator>
+	bool operator<=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+		return (!(rhs < lhs));
+	};
+
+	template <class Iterator>
+	bool operator>(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+		return (rhs < lhs);
+	};
+
+	template <class Iterator>
+	bool operator>= (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+		return (!(lhs < rhs));
+	};
+
+	template <class Iterator>
+	typename reverse_iterator<Iterator>::difference_type operator-(const reverse_iterator<Iterator>& lhs,
+																   const reverse_iterator<Iterator>& rhs) {
+		return (rhs.base() - lhs.base());
+	};
+
+	template <class Iterator>
+	reverse_iterator<Iterator> operator+(typename reverse_iterator<Iterator>::difference_type n,
+										 const reverse_iterator<Iterator>& rev_it) {
+		return (reverse_iterator<Iterator>(rev_it.base() - n));
+	};
+
 }; //namespace ft
 
 #endif
