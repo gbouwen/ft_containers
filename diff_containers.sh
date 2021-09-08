f513851056b225d9e3c27179a653e60416556131
gbouwen <gijsbouwen@gmail.com>
2021-09-08 15:53:12 +0200

    vector capacity tests

--- diff_containers.sh
+++ diff_containers.sh
@@ -1,3 +1,3 @@
 diff tests/stack/output/outputMyStack.txt tests/stack/output/outputStack.txt
-#diff tests/stack/output/outputMyVector.txt tests/stack/output/outputVector.txt
+diff tests/vector/output/outputMyVector.txt tests/vector/output/outputVector.txt
 #diff tests/stack/output/outputMyMap.txt tests/stack/output/outputMap.txt
--- tests/vector/cpp_files/stl_vector_tests.cpp
+++ tests/vector/cpp_files/stl_vector_tests.cpp
@@ -52,7 +52,7 @@ void vector_equals_overload_1()
 	std::cout << "test_2.size() = " << test_2.size() << std::endl;
 }
 
-void vector_constructor()
+void vector_constructors()
 {
 	struct timeval	start, end;
 
@@ -130,11 +130,149 @@ void vector_iterators()
 	calc_time_taken(start, end, "stl_vector.iterators()");
 }
 
+void vector_size_1()
+{
+	int_vector test;
+
+	std::cout << "test.size() = " << test.size() << std::endl;
+	for (int i = 0; i < 1000; i++)
+		test.push_back(i);
+	std::cout << "test.size() = " << test.size() << std::endl;
+}
+
+void vector_size_2()
+{
+	int_vector test;
+
+	std::cout << "test.size() = " << test.size() << std::endl;
+	for (int i = 0; i < 1000; i++)
+		test.push_back(i);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	for (int i = 0; i < 123; i++)
+		test.pop_back();
+	std::cout << "test.size() = " << test.size() << std::endl;
+}
+
+void vector_max_size_1()
+{
+	int_vector		test_1;
+	string_vector	test_2;
+
+	std::cout << "test_1.max_size() = " << test_1.max_size() << std::endl;
+	std::cout << "test_2.max_size() = " << test_2.max_size() << std::endl;
+}
+
+void vector_resize_1()
+{
+	int_vector test;
+
+	for (int i = 0; i < 10; i++)
+		test.push_back(i);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	test.resize(5);
+	std::cout << "test.size() = " << test.size() << std::endl;
+}
+
+void vector_resize_2()
+{
+	int_vector test;
+
+	for (int i = 0; i < 5; i++)
+		test.push_back(i);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	for (size_t i = 0; i < test.size(); i++)
+		std::cout << test[i];
+	std::cout << std::endl;
+	test.resize(10, 5);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	for (size_t i = 0; i < test.size(); i++)
+		std::cout << test[i];
+	std::cout << std::endl;
+}
+
+void vector_resize_3()
+{
+	int_vector test;
+
+	for (int i = 0; i < 5; i++)
+		test.push_back(i);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	for (size_t i = 0; i < test.size(); i++)
+		std::cout << test[i];
+	std::cout << std::endl;
+	test.resize(50, 5);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	for (size_t i = 0; i < test.size(); i++)
+		std::cout << test[i];
+	std::cout << std::endl;
+}
+
+void vector_capacity_1()
+{
+	int_vector test;
+
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	for (int i = 0; i < 5; i++)
+		test.push_back(i);
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	test.resize(50, 5);
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	test.resize(5);
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+}
+
+void vector_empty_1()
+{
+	int_vector test;
+
+	std::cout << "test.empty() = " << test.empty() << std::endl;
+	for (int i = 0; i < 5; i++)
+		test.push_back(i);
+	std::cout << "test.empty() = " << test.empty() << std::endl;
+}
+
+void vector_reserve_1()
+{
+	int_vector test;
+
+	std::cout << "test.size() = " << test.size() << std::endl;
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	test.reserve(100);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	for (int i = 0; i < 20; i++)
+		test.push_back(i);
+	std::cout << "test.size() = " << test.size() << std::endl;
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+	test.reserve(20);
+	std::cout << "test.capacity() = " << test.capacity() << std::endl;
+}
+
+void vector_capacity()
+{
+	struct timeval	start, end;
+
+	gettimeofday(&start, NULL);
+	vector_size_1();
+	vector_size_2();
+	vector_max_size_1();
+	vector_resize_1();
+	vector_resize_2();
+	vector_resize_3();
+	vector_capacity_1();
+	vector_empty_1();
+	vector_reserve_1();
+	gettimeofday(&end, NULL);
+	calc_time_taken(start, end, "stl_vector.capacity()");
+}
+
 void vector_main(void)
 {
-//	vector_constructors();
+	vector_constructors();
 	vector_iterators();
-//	vector_capacity();
+	vector_capacity();
 //	vector_element_access();
 //	vector_modifiers();
 //	vector_relational_operators();
